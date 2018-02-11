23194e6704f1a5aebc3029defb07cec996f2c623
Michael Gerakis <mgerakis@bu.com>
2018-02-11 18:58:47 -0500

    Add a minimal, dependency-free version of Rusts standard libary.
    The code is taken from the CS140e coursework by Sergio Benitez.
    Also add an unimplemented Console interface and a basic Mutex structure.

--- src/kmain.rs
+++ src/kmain.rs
@@ -1,11 +1,19 @@
-#![feature(compiler_builtins_lib, lang_items, asm, pointer_methods)]
-#![no_builtins]
-#![no_std]
+#![feature(compiler_builtins_lib)]
+#![feature(lang_items)]
+#![feature(pointer_methods)]
+#![feature(asm)]
+#![feature(repr_align)]
+#![feature(const_fn)]
+#![feature(attr_literals)]
+#![feature(optin_builtin_traits)]
+#![feature(decl_macro)]
 
-extern crate compiler_builtins;
+extern crate core;
 extern crate volatile;
 
 pub mod lang_items;
+pub mod mutex;
+pub mod console;
 pub mod common;
 pub mod timer;
 pub mod gpio;
